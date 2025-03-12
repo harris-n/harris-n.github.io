303653c904a9c8b606556429fd4cdf96b29c578f
harris-n <harris.n2703@gmail.com>
2025-03-12 17:05:03 +1100

    Rewrite of puzzle generation rng

--- index.js
+++ index.js
@@ -1,22 +1,58 @@
-var gridSize = 7;
+// Make grid
 const resetButton = document.getElementById('reset-button');
 const gridDiv = document.getElementById('grid-div');
 
-for (let rowNum = 0; rowNum < gridSize; rowNum++) {
-    const newGridRow = document.createElement('div');
-    newGridRow.id = `${rowNum}`;
-    newGridRow.classList.toggle('grid-row');
+const timerTextbox = document.getElementById("timer-textbox");
 
-    for (let colNum = 0; colNum < gridSize; colNum++) {
-        const newGridTile = document.createElement('div');
-        newGridTile.classList.toggle('grid-tile');
-        newGridTile.id = `${rowNum},${colNum}`;
-        newGridRow.append(newGridTile);
+// RNG seed thing
+
+class MersenneTwister {
+    constructor(seed) {
+      this.state = new Array(624);
+      this.index = 0;
+      this.initialize(seed);
     }
-
-    gridDiv.append(newGridRow);    
+  
+    initialize(seed) {
+      this.state[0] = seed >>> 0;
+      for (let i = 1; i < 624; i++) {
+        let s = this.state[i - 1] ^ (this.state[i - 1] >>> 30);
+        this.state[i] = ((0x6c078965 * s) + i) >>> 0;
+      }
+    }
+  
+    twist() {
+      for (let i = 0; i < 624; i++) {
+        let y = (this.state[i] & 0x80000000) + (this.state[(i + 1) % 624] & 0x7fffffff);
+        this.state[i] = this.state[(i + 397) % 624] ^ (y >>> 1);
+        if (y % 2 !== 0) {
+          this.state[i] ^= 0x9908b0df;
+        }
+      }
+      this.index = 0;
+    }
+  
+    extract() {
+      if (this.index === 0) {
+        this.twist();
+      }
+      let y = this.state[this.index];
+      y ^= (y >>> 11);
+      y ^= (y << 7) & 0x9d2c5680;
+      y ^= (y << 15) & 0xefc60000;
+      y ^= (y >>> 18);
+      this.index = (this.index + 1) % 624;
+      return y >>> 0;
+    }
+  
+    next() {
+      return this.extract() % 4;
+    }
 }
 
+/* ##########################################################################################
+ADJUSTING SETTINGS
+########################################################################################## */
 const gridTiles = document.querySelectorAll('.grid-tile');
 var totalMoves = 0;
 var minMoves = 0;
@@ -84,87 +120,284 @@ Moves Taken: ${totalMoves}${minMoves}
 Move Efficency: ${efficencyRate}%
 */
 
+const gridRowInput = document.getElementById('row-dim-input');
+const gridColInput = document.getElementById('col-dim-input');
+  
+  
+/* ##########################################################################################
+CREATING THE PUZZLE
+########################################################################################## */
 
 class tile {
-    constructor(rowNum, colNum) {
+    constructor(rowNum, colNum, puzzle) {
         this.rowNum = rowNum;
         this.colNum = colNum;
         this.connections = [false, false, false, false];
         this.visited = false;
         this.type;
-        this.solRotation;
-        this.currentRotation;
-        this.addedRotation;
+        this.solRotation = 0;
+        this.addedRotation = 0;
+        this.currentRotation = 0;
+        this.timesClicked = 0;
+        this.div;
+        this.puzzle = puzzle;
     }
 
-    checkNeighbours() {
-        const neighbours = [];
+    render(puzzle) {
+        const div = document.createElement('div');
+        div.id = `${this.rowNum},${this.colNum}`;
+        div.className = 'grid-tile';
+        div.addEventListener('mousedown', (event) => this.rotateTile(event));
 
-        const i = this.rowNum;
-        const j = this.colNum;
+        // Find tile type
+        let tileType = tileDictionary[this.connections.join(',')]
+        this.type = tileType.type;
+        this.solRotation = tileType.rotation;
+        this.currentRotation += tileType.rotation;
 
-        if (i - 1 > -1 && grid[i-1][j].visited == false) {
-            neighbours.push(grid[i-1][j]);
+        div.style.backgroundImage = `url(sprite/${this.type}-pipe.png)`;
+
+        // Add random rotation
+        var rndNum = puzzle.rng.next();
+
+        if (this.type !== 'straight') {
+            this.addedRotation = rndNum;
+            this.currentRotation += rndNum; 
+            puzzle.minMoves += (4 - rndNum) % 4;
+        } else {
+            this.addedRotation = rndNum % 2;
+            this.currentRotation += rndNum % 2;
+            puzzle.minMoves += (4 - rndNum) % 2;
         }
-        if (j + 1 < gridSize && grid[i][j + 1].visited == false) {
-            neighbours.push(grid[i][j + 1]);
+
+        div.style.transform = `rotate(${this.currentRotation*90}deg)`;
+        this.div = div;
+
+        return div;
+    }
+
+    chooseNeighbour() {
+        let neighbours = [];
+
+        let i = this.rowNum;
+        let j = this.colNum;
+
+        if (i - 1 > -1 && !this.puzzle.gridTiles[i-1][j].visited) {
+            neighbours.push(this.puzzle.gridTiles[i-1][j]);
         }
-        if (i + 1 < gridSize && grid[i+1][j].visited == false) {
-            neighbours.push(grid[i+1][j]);
+        if (j + 1 < this.puzzle.colNum && !this.puzzle.gridTiles[i][j + 1].visited) {
+            neighbours.push(this.puzzle.gridTiles[i][j + 1]);
         }
-        if (j - 1 > -1 && grid[i][j-1].visited == false) {
-            neighbours.push(grid[i][j-1]);
+        if (i + 1 < this.puzzle.rowNum && !this.puzzle.gridTiles[i+1][j].visited) {
+            neighbours.push(this.puzzle.gridTiles[i+1][j]);
         }
+        if (j - 1 > -1 && !this.puzzle.gridTiles[i][j-1].visited) {
+            neighbours.push(this.puzzle.gridTiles[i][j-1]);
+        }
+
 
         if (neighbours.length > 0) {
-            let rndNeighbour = Math.floor(Math.random() * neighbours.length);
+            let rndNeighbour = this.puzzle.rng.next() % neighbours.length;
             return neighbours[rndNeighbour];
         } else {
             return undefined;
         }
-        
     }
 
-    setPipe() {
-        var key = this.connections.join(',');
-        var thisTileInfo = tileDictionary[key];
-        this.solRotation = thisTileInfo.rotation;
-        this.type = thisTileInfo.type;
+    addConnection(nextTile) {
+        let di = this.rowNum - nextTile.rowNum;
+        let dj = this.colNum - nextTile.colNum;
+        
+        if (di == -1) {
+            // current tile is above next tile
+            this.connections[2] = true;
+            nextTile.connections[0] = true;
+        } else if  (di == 1) {
+            // current tile is below next tile
+            this.connections[0] = true;
+            nextTile.connections[2] = true;
+        } else if (dj == -1) {
+            // current tile is left of the next tile
+            this.connections[1] = true;
+            nextTile.connections[3] = true;
+        } else if (dj == 1) {
+            // current tile is right of the next tile
+            this.connections[3] = true;
+            nextTile.connections[1] = true;
+        }
+    }
 
-        var i = this.rowNum;
-        var j = this.colNum;
 
-        if (sizeSlider.value == 90) {
-            spriteTheme = 'cat';
-        } else if (sizeSlider.value == 10) {
-            spriteTheme = 'frog';
-        } else if (sizeSlider.value == 69) {
-            spriteTheme = 'rabbit';
-        }    
+    rotateTile(){
 
-
-        document.getElementById(`${i},${j}`).style.backgroundImage = `url(sprite/${spriteTheme}/${this.type}-pipe.png)`;
-        var rndNum = Math.floor(Math.random() * 4);
-        // for fun, rotates the tiles to its actual orientation
-        // document.getElementById(`${i},${j}`).style.transform = `rotate(${this.solRotation*90}deg)`;
-        this.addedRotation = rndNum;
-
-        if (this.type !== 'straight') {
-            this.currentRotation = this.solRotation + rndNum;
-            // document.getElementById(`${i},${j}`).textContent = (4 - rndNum) % 4;
-            minMoves += (4 - rndNum) % 4;
-        } else {
-            this.currentRotation = this.solRotation + rndNum % 2;
-            // document.getElementById(`${i},${j}`).textContent = rndNum % 2;
-            minMoves += (4 - rndNum) % 2;
+        if (this.puzzle.timeTaken > 0) {
+            return
         }
 
-        document.getElementById(`${i},${j}`).style.transform = `rotate(${this.currentRotation*90}deg)`;
+        this.timesClicked += 1;
 
+        if (this.type !== 'straight') {
+            this.currentRotation = (this.currentRotation + 1) % 4;
+        } else {
+            this.currentRotation = (this.currentRotation + 1) % 2;
+        }
+        
+        this.div.style.transform = `rotate(${this.currentRotation*90}deg)`;
+
+        this.puzzle.checkGrid();
     }
 }
 
-var spriteTheme = 'pink'
+
+class puzzle {
+    constructor(rowNum, colNum, seed = 0) {
+        this.rowNum = rowNum
+        this.colNum = colNum
+        this.gridTiles = this.generateGrid(rowNum, colNum);    
+        this.rng = this.generateRNG(seed);
+        this.minMoves = 0;
+        this.startTime = null;
+        this.finishTime;
+        this.timer;
+        this.timeTaken = 0;
+        this.puzzleModal = document.getElementById('puzzle-dialog');
+        this.generatePuzzle();
+        this.render();
+    }
+
+    generatePuzzle() {
+        // DFS maze generation method
+        var currentTile = this.gridTiles[this.rng.next()][this.rng.next()];
+        var stackTiles = [];
+        var finishGenerating = false; 
+
+        while (!finishGenerating) {
+            currentTile.visited = true;
+            var nextTile = currentTile.chooseNeighbour();
+
+            if (nextTile) {
+                nextTile.visited = true;
+                stackTiles.push(currentTile);
+                currentTile.addConnection(nextTile);
+                currentTile = nextTile;
+            } else if (stackTiles.length > 0) {
+                while (this.checkFourway(stackTiles)) {
+                    stackTiles.pop();
+                }
+                currentTile = stackTiles.pop();
+            } else {
+                finishGenerating = true;
+            }   
+        }
+    }
+
+    checkFourway(stackTiles) {
+        var trueCount = 0;
+        var lastStackTile = stackTiles[stackTiles.length-1];
+     
+        for (let i = 0; i < 4; i++) {
+            if (lastStackTile.connections[i] == true) {
+                trueCount++;
+            }
+        }
+        if (trueCount >= 3) {
+            return true;
+        } 
+        return false;
+    }
+
+    generateRNG(seed) {
+        if (seed == 0) {
+            seed = Math.round(Math.random() * 10000000);
+        }
+
+        console.log(seed)
+        let mt = new MersenneTwister(seed);
+        return mt;
+    }
+
+    generateGrid(rowNum, colNum) {
+        let grid = [];
+        for (let i = 0; i < rowNum; i++) {
+            let row = [];
+            for (let j = 0; j < colNum; j++) {
+                row.push(new tile(i,j, this));
+            }
+            grid.push(row);
+        }
+        return grid;
+    }
+
+    render() {
+        this.gridTiles.forEach(row => {
+            const rowDiv = document.createElement('div');
+            rowDiv.className = 'grid-row';
+
+            row.forEach(tile => {
+                const tileDiv = tile.render(this);
+                rowDiv.appendChild(tileDiv);
+            });
+            gridDiv.appendChild(rowDiv);
+        });
+
+
+        this.puzzleModal.addEventListener("mousedown", () => {
+
+            if (this.timeTaken == 0) {
+                this.startTime = Date.now();
+                this.startTimer()    
+            }
+
+            this.puzzleModal.close();
+        });
+
+    }
+
+    checkGrid() {
+
+        correctTiles = 0;
+
+        this.gridTiles.forEach(element => {
+            element.forEach((element) => {
+                if (element.currentRotation == element.solRotation) {
+                    correctTiles += 1;
+                }
+            })
+          
+        });
+
+        if (correctTiles == (this.rowNum * this.colNum)) {
+            console.log(this); 
+        
+            this.showResults();
+        }
+    }
+
+    startTimer() {
+        this.timer = setInterval(this.updateTime, 1);
+    }
+
+    updateTime() {
+
+        if (this.startTime == null) {
+            this.startTime = Date.now();
+        }
+
+        var currentTime = Date.now();
+        timerTextbox.textContent = currentTime - this.startTime;
+
+    }
+
+    showResults() {
+        this.puzzleModal.showModal();
+        this.finishTime = Date.now();
+        this.timeTaken = this.finishTime - this.startTime;
+        clearInterval(this.timer);
+    }
+
+}
+
+var correctTiles = 0;
 
 const tileDictionary = {
     'true,false,false,false':  {type: 'end',          rotation: 0},
@@ -289,131 +522,84 @@ function addConnection(currentTile, nextTile) {
         currentTile.connections[3] = true;
         nextTile.connections[1] = true;
     }
+    
 }
 
 
+/* ##########################################################################################
+Puzzle Logic
+########################################################################################## */
 
-window.addEventListener('resize', adjustDivSize);
-window.addEventListener('load', startup);
 
-function adjustDivSize() {
-    const targetSize = (sizeSlider.value/100) * Math.min(window.innerHeight, window.innerWidth);
-    const gridDimension = Math.round(targetSize / gridSize) * gridSize;
-
-    gridDiv.style.height = `${gridDimension}px`;
-    gridDiv.style.width = `${gridDimension}px`;
-
-    gridOverlay.style.height = gridDiv.style.height;
-    gridOverlay.style.width = gridDiv.style.width;
-    gridOverlay.style.display = 'none';
-}
-
-function startup() {
-    adjustDivSize();
-    displayPersonalBest();
-}
-
-const gridOverlay = document.getElementById('grid-overlay');
-var timeStarted;
-
-gridOverlay.addEventListener('mousedown', () => {
-    if (gameStart) {
-        gridOverlay.style.display = 'none';
-        timeStarted = Date.now();
-    }
-});
-
-const copyResultsButton = document.getElementById('copy-results-button');
-copyResultsButton.addEventListener('mousedown', () => {
-  navigator.clipboard.writeText(`I did a pipes
-╔════════════
-║Time Taken: ${timeTaken}s
-║Efficiency: ${efficencyRate}%
-║https://harris-n.github.io/ 
-╚════════════════`);
-  debug("Copied Text")
-});
-
-/*
-Finished!
-Time Taken: ${timeTaken}
-Moves Taken: ${totalMoves}${minMoves}
-Move Efficency: ${efficencyRate}%
-*/
-
-const pipeHeader = document.getElementById('pipe-title');
-
-function getAspectRatio(src, callback) {
-    var img = new Image();
-    img.onload = function() {
-        var aspectRatio = img.naturalWidth / img.naturalHeight;
-        callback(aspectRatio);
-    };
-    img.src = src;
-}
-
-getAspectRatio('sprite/pipes-header-rainbow.png', function(aspectRatio) {
-    var pipeHeader = document.getElementById('pipe-title');
-    var newHeight = 0.1 * Math.min(window.innerHeight, window.innerWidth);
-    pipeHeader.style.height = newHeight + "px";
-    pipeHeader.style.width = (newHeight * aspectRatio) + "px";
-});
-
+/* ##########################################################################################
+ADJUSTING GRID SIZE RELATIVE TO WINDOW
+########################################################################################## */
 
 const sizeSlider = document.getElementById('size-input');
-const sizeText = document.getElementById('size-textbox');
 
 sizeSlider.addEventListener("input", () => {
     adjustDivSize();
-    sizeText.textContent = sizeSlider.value;
 });
 
-const PbResetButton = document.getElementById('pb-reset-button');
-const PbTextbox = document.getElementById('pb-textbox');
-
-PbResetButton.addEventListener("click", () => {
-    localStorage.removeItem("personalBest");
-    displayPersonalBest();
-    debug("Pb reset");
+window.addEventListener('resize', adjustDivSize);
+window.addEventListener('load', () =>{
+    gridDiv.style.width = '60vmin';
+    gridDiv.style.height = '60vmin';
 });
 
-function displayPersonalBest() {
-    personalBestTime = Number(localStorage.getItem("personalBest"));
-    PbTextbox.textContent = `Personal Best Time: ${personalBestTime}s`;
+function adjustDivSize() {
 
-    if (!personalBestTime) {
-        localStorage.setItem("personalBest", "Not Set");
-        PbTextbox.textContent = `Personal Best Time: Not Set`;
-    }    
+    gridDiv.style.width = 'auto';
+    gridDiv.style.height = 'auto';
 
+    var rowDim = gridRowInput.value;
+    var colDim = gridColInput.value;
+
+    const winHeight = Math.round(window.innerHeight * (sizeSlider.value/100) / colDim);
+    const winWidth = Math.round(window.innerWidth * (sizeSlider.value/100) / rowDim);
+
+    const tileSize = Math.min(winWidth, winHeight);
+    
+    const gridTiles = document.querySelectorAll('div.grid-tile');
+
+    gridTiles.forEach((gridTile) => {
+        gridTile.style.height = `${tileSize}`;
+        gridTile.style.window = `${tileSize}`;
+    })
+
+    PuzzleRect = gridDiv.getBoundingClientRect();
+
+    puzzleModal.style.width = `${PuzzleRect.width}px`;
+    puzzleModal.style.height = `${PuzzleRect.height}px`;
+    puzzleModal.style.top = `${PuzzleRect.top}px`;
+    puzzleModal.style.left = `${PuzzleRect.left}px`;
 }
 
-function updatePersonalBest(timeTaken) {
-    personalBestTime = localStorage.getItem("personalBest");
-    if (timeTaken < personalBestTime || personalBestTime == "Not Set") {
-        overlayMessage.textContent += ("\nNew Personal Best!");
-        localStorage.setItem("personalBest", timeTaken);
+var currentPuzzle;
+const puzzleModal = document.getElementById('puzzle-dialog');
+var PuzzleRect;
+
+resetButton.addEventListener("mouseup", () => {
+
+    var rowDim = gridRowInput.value;
+    var colDim = gridColInput.value;
+
+    while (gridDiv.firstChild) {
+        gridDiv.removeChild(gridDiv.firstChild);
     }
-    displayPersonalBest();
-}
 
-const debugTextbox = document.getElementById("debug-textbox");
-var debugtext = debugTextbox.textContent;
+    currentPuzzle = new puzzle(rowDim, colDim, 10000000); // Creates a 4x4 grid
+    adjustDivSize();
 
-function debug(text) {
-    debugtext += `\n${text}`;
-    debugTextbox.textContent = debugtext;     
-}
+    PuzzleRect = gridDiv.getBoundingClientRect();
 
-const settingsButton = document.getElementById("settings-button");
-const settingsDiv = document.getElementById("setting-div");
-var hideSettings = true;
+    puzzleModal.style.width = `${PuzzleRect.width}px`;
+    puzzleModal.style.height = `${PuzzleRect.height}px`;
+    puzzleModal.style.top = `${PuzzleRect.top}px`;
+    puzzleModal.style.left = `${PuzzleRect.left}px`;
+    
+    puzzleModal.showModal();
 
-settingsButton.addEventListener("click", () => {
-    hideSettings = !hideSettings;
-    if (hideSettings) {
-        settingsDiv.style.display = "none";
-    } else {
-        settingsDiv.style.display = "flex";
-    }
+    console.log(currentPuzzle);
 });
+
